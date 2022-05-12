27e78b12e2ffe674e6e371d7f978e37cc8214a19
mbpapazova <104827282+mbpapazova@users.noreply.github.com>
2022-05-12 10:34:56 +0300

    Update Hall.cpp

--- Hall.cpp
+++ Hall.cpp
@@ -4,20 +4,101 @@ class Hall {
 private:
 
     int numberOfHall;
-    int numberOfLines;
+    int numberOfRows;
     int numberOfSeats;
-
+    int seats[20][20];
 
 public:
 
-    Hall(int numberOfHall, int numberOfLines, int numberOfSeats);
+    Hall(){
+        numberOfHall = 0;
+        numberOfRows = 0;
+        numberOfSeats = 0;
+
+        this->setSeats();
+    }
+
+    Hall(int numberOfHall, int numberOfRows, int numberOfSeats) {
+        this->numberOfHall = numberOfHall;
+        this->numberOfRows = numberOfRows;
+        this->numberOfSeats = numberOfSeats;
+
+        this->setSeats();
+    }
+
+    Hall(const Hall& other){
+        numberOfHall = other.numberOfHall;
+        numberOfRows = other.numberOfRows;
+        numberOfSeats = other.numberOfSeats;
+        
+        for(int i=0; i < this->numberOfRows; i++){
+            for(int j=0; j < this->numberOfSeats; j++) {
+                this->seats[i][j] = other.seats[i][j];
+            }
+        }
+    }
+
+    Hall& operator=(const Hall& other){
+        if(this != &other){
+            numberOfHall = other.numberOfHall;
+            numberOfRows = other.numberOfRows;
+            numberOfSeats = other.numberOfSeats;
+            
+            for (int i = 0; i < this->numberOfRows; i++) {
+                for (int j = 0; j < this->numberOfSeats; j++) {
+                    this->seats[i][j] = other.seats[i][j];
+                }
+            }
+        }
+        return *this;
+    }
 
     int getNumberOfHall() const { return numberOfHall; } 
-    int getNumberOfLines() const { return numberOfLines; } 
+    int getNumberOfRows() const { return numberOfRows; } 
     int getNumberOfSeats() const { return numberOfSeats; } 
 
-   friend bool operator==(const Hall& l, const Hall& r) {
+    void setSeats() {
+        for (int i = 0; i < this->numberOfRows; i++) {
+            for (int j = 0; j < this->numberOfSeats; j++) {
+                this->seats[i][j] = 0;
+            }
+        }
+    }
+
+
+   void getFreeSeats() {
+        std::cout<< "Free seats: " << std::endl;
+
+        for(int i=0; i < numberOfRows; i++){
+            for(int j=0; j < numberOfSeats; j++) {
+                if(this->seats[i][j] == 0) {
+                   std::cout << "SEAT AT ROW: " << i << ", NUM: " << j << " is free" << std::endl; 
+                }
+            }
+        }
+    }
+
+   void printFreeSeats() {
+       for (int i = 0; i < numberOfRows; i++) {
+           for (int j = 0; j < numberOfSeats; j++) {
+               if (this->seats[i][j] == 0) {
+                   std::cout << "0 ";
+               }
+               else {
+                   std::cout << "1 ";
+               }
+           }
+           std::cout << std::endl;
+       }
+   }
+
+    friend bool operator==(const Hall& l, const Hall& r) {
         return l.getNumberOfHall() == r.getNumberOfHall();
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Hall& hall){
+         os<< hall.getNumberOfHall() << " " << hall.getNumberOfRows() << " " << hall.getNumberOfSeats() << std::endl;
+    return os;
     }
 
 };
