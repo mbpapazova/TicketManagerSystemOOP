42d8a5cf36c746e0b0678b75b98fc3fc62f2acb9
mbpapazova <104827282+mbpapazova@users.noreply.github.com>
2022-05-12 10:39:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
+#include <cstring>
 #include "TicketManager.cpp"
+using namespace std;
+
 
 int main() {
+    Hall h1 = { 1, 6, 8 };
+    Hall h2 = { 2, 3, 12 };
+    Hall h3 = { 3, 3, 12 };
 
-    Hall h1 = {1, 2, 8};
-    Hall h2 = { 2, 3, 12};
+    TicketManager manager(10);
 
-    TicketManager manager(5);
+    char* movie1 = new char[10];
+    strcpy_s(movie1, 10, "Spiderman");
+    manager.addNewEvent(movie1, 1, h1);
 
-    manager.addNewEvent(1, "Spinderman", h1);
+    char* movie2 = new char[7];
+    strcpy_s(movie2, 7, "Batman");
+    manager.addNewEvent(movie2, 3, h1);
 
+    char* movie3 = new char[8];
+    strcpy_s(movie3, 8, "Ironman");
+    manager.addNewEvent(movie3, 3, h2);
+
+    std::cout << " ----------- " << std::endl;
     manager.printAll();
+    std::cout << " ----------- " << std::endl;
+  
+    h1.printFreeSeats();
+    h2.printFreeSeats();
 
+    manager.freeSeats(movie1, 1);
 
+    manager.freeSeats(movie2, 3);
+
+    return 0;
 
 }
